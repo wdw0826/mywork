764c449ae631dd73e78c86440637fd5ac7eeb40a
wdw0826 <wdw0826@gmail.com>
2026-04-20 21:54:53 +0800

    just test

---
+++ untitled/src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        for(int i=1;i<=1000;i++){
+            if(i%17==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
